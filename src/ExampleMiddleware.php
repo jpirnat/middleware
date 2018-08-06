5f6f2a5b0eb42c796eca10f303b00c527c2e8ed3
Jesse Pirnat <jessepirnat@gmail.com>
2018-08-06 14:58:34 -0400

    Updating interfaces for the official PSR-17 release.

--- src/ExampleMiddleware.php
+++ src/ExampleMiddleware.php
@@ -3,7 +3,7 @@ declare(strict_types=1);
 
 namespace Jp\Middleware;
 
-use Interop\Http\Factory\ResponseFactoryInterface;
+use Psr\Http\Message\ResponseFactoryInterface;
 use Psr\Http\Message\ResponseInterface;
 use Psr\Http\Message\ServerRequestInterface;
 use Psr\Http\Server\MiddlewareInterface;
